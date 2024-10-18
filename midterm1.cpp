7e5dbe2c51bd7830f05eb85db93de145e3c09187
me1lof <melissaochoaf04@gmail.com>
2024-10-17 19:26:47 -0700

    Add delete_val method

--- midterm1.cpp
+++ midterm1.cpp
@@ -1,20 +1,88 @@
-// midterm1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Melissa Ochoa FLores
+// COMSC 210
+// Midterm 1
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+// A structure to represent each node in the doubly linked list
+struct Node {
+    int data; // Holds the integer value of the node
+    Node* prev; // Pointer to the previous node in the list
+    Node* next; // Pointer to the next node in the list
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    // Constructor to initialize the node with a value and optional pointers to previous and next nodes
+    Node(int val, Node* p = nullptr, Node* n = nullptr) {
+        data = val;
+        prev = p;
+        next = n;
+    }
+};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Doubly linked list class to manage nodes
+class DoublyLinkedList {
+private:
+    Node* head; // Points to the first node
+    Node* tail; // Points to the last node
+
+public:
+    // Constructor initializes an empty doubly linked list
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void push_back(int v) {
+        // Create a new node with the given value
+        Node* newNode = new Node(v);
+        if (!tail) {
+            
+            head = tail = newNode;
+        }
+        else {
+            // Link the new node at the end of the list
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;  // Update the tail pointer to the new node
+        }
+    }
+    void push_front(int v) {
+        // Create a new node with the provided value
+        Node* newNode = new Node(v);
+        if (!head) {
+            
+            head = tail = newNode;
+        }
+        else {
+            
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;  // Update head to point to the new node
+        }
+    }
+    void print() {
+        Node* current = head;  // Start from the head node
+        if (!current) {
+            cout << "List is empty." << endl;  // Handle empty list case
+            return;
+        }
+
+        // print each node's data
+        while (current) {
+            cout << current->data << " ";  // Print the data of the current node
+            current = current->next;  // Move to the next node
+        }
+        cout << endl;  
+    }
+
+    void delete_val(int value) {
+        if (!head) return;  // Return if the list is empty
+
+        Node* temp = head;  // Start from the head of the list
+        while (temp && temp->data != value) {
+            temp = temp->next;  
+        }
+
+        if (!temp) return;
+
+        // This is a test comment to check Git status
